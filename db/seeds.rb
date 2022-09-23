29b5f14a229ce1d7dfc6edb5066a9637584bda21
ElijahBcodes <eli1inthesnow@gmail.com>
2022-09-23 15:11:11 -0700

    rspec and seeds comp

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+cats = [ {
+    id: 1,
+    name: "Lindo",
+    age: 4,
+    enjoys: "Enjoys sleeping and being a bum. Don't expect much from him. Minimal effort type of catdude.",
+    image:
+      "https://images.unsplash.com/photo-1597237154674-1a0d2274cef4?ixlib=rb-1.2.1&ixid=MnwxM[…]jBjYXRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+  },
+
+  {
+    id: 2,
+    name: "meowcycles",
+    age: 7,
+    enjoys: "A calico, more like caliwoah. Enjoys working out and flexing making other cats sweat.  ",
+    image:
+      "https://i1.sndcdn.com/artworks-000457187166-b1k6d8-t500x500.jpg" 
+  }
+]
+
+cats.each do | each_cat |
+    Cat.create each_cat
+    puts "creating cat #{each_cat}"
+end
